b7404d80b9ad055c52b5f29511c1e1240c2b99c1
Littlebath <Littlebath@users.noreply.github.com>
2019-05-09 00:18:36 +0530

    Shop entrance and exit made

---
+++ Sweet Tooth/Assets/Scripts/Shop/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    private PlayerInput pi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pi = gameObject.GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shop_Exit();
+    }
+
+    void Shop_Exit ()
+    {
+        if (pi.attackButton)
+        {
+            Debug.Log("Exit Scene");
+            StartCoroutine(gameObject.GetComponent<LoadNewArea>().GoToNextLevel());
+        }
+    }
+}
