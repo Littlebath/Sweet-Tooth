1c4f031de819e08bc030fec0eb158b92f7c064a7
Littlebath <Littlebath@users.noreply.github.com>
2019-06-10 10:17:55 +0530

    Dialogue trigger finished

---
+++ Sweet Tooth/Assets/Environment_DialogueTrigger.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment_DialogueTrigger : MonoBehaviour
+{
+
+    //Trigger ID
+    public GameObject allDialogues;
+
+
+    //Boolean for dialogue
+    private bool activateTrigger;
+
+    private int dialogueSelector;
+
+    //Gameobjects
+    private GameObject player;
+
+    //Scripts
+    private PlayerInput pi;
+    private Manager_Dialogue md;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SaveFeature();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Find_Objects();
+
+        if (activateTrigger)
+        {
+            if (pi.interactButton)
+            {
+                Debug.Log("Button pressed");
+
+                if (FindObjectOfType<PlayerController>().isMoving == false)
+                {
+                    if (!md.isTalking)
+                    {
+                        FindObjectOfType<PlayerController>().isMelee = false;
+                        Debug.Log("Start dialogue");
+                        GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                        //gameObject.GetComponent<Dialogue_Trigger>().TriggerDialogue();
+                        //Debug.Log("Talk");
+                    }
+
+                    else
+                    {
+
+                        if (md.isSpeaking)
+                        {
+                            md.SkipDialogue();
+                        }
+
+                        else
+                        {
+                            md.DisplayNextSentence();
+                        }
+
+                        if (md.anim.GetBool("isOpen") == false)
+                        {
+                            gameObject.GetComponent<Save_ObjState>().obj.saveState = 1;
+                            gameObject.GetComponent<Save_ObjState>().obj.ForceSerialization();
+                            gameObject.SetActive(false);
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    void Find_Objects()
+    {
+        if (pi == null)
+        {
+            pi = FindObjectOfType<PlayerInput>();
+        }
+
+        if (md == null)
+        {
+            md = FindObjectOfType<Manager_Dialogue>();
+        }
+
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+
+    private void SaveFeature()
+    {
+        if (gameObject.GetComponent<Save_ObjState>() != null)
+        {
+            if (gameObject.GetComponent<Save_ObjState>().obj.saveState == 1)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            activateTrigger = true;
+
+            if (!md.isTalking)
+            {
+                FindObjectOfType<PlayerController>().isMelee = false;
+                Debug.Log("Start dialogue");
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                Say_Dialogue();
+                //Debug.Log("Talk");
+            }
+        }
+    }
+
+    void Say_Dialogue()
+    {
+        //Underground Part A Cave entrance
+        player.GetComponent<Animator>().SetBool("isMoving", false);
+        dialogueSelector = 0;
+        allDialogues.transform.GetChild(dialogueSelector).GetComponent<Dialogue_Trigger>().TriggerDialogue();
+    }
+}
